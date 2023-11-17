077f0a917efef0c6221c9d46f798e0e0d00fd982
MohitD9 <deshpandemohit1999@gmail.com>
2023-11-17 09:33:15 +0530

    add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
+import Error from "./pages/Error";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={<Home />} />
-        <Route path="/success" Component={<Success />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );

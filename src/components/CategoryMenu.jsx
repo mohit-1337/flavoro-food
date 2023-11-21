b3eb3bc6ac50d49ea5b014348a5bf89af7f8a9a7
MohitD9 <deshpandemohit1999@gmail.com>
2023-11-21 16:57:02 +0530

    category added

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,23 +1,34 @@
+import { useState, useEffect } from "react";
+import FoodData from "../data/FoodData";
+
 const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+  };
+
+  useEffect(() => {
+    listUniqueCategories();
+  }, []);
+
   return (
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best Food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
-        <button className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white">
-          All
-        </button>
-        <button className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white">
-          Breakfast
-        </button>
-        <button className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white">
-          Lunch
-        </button>
-        <button className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white">
-          Dinner
-        </button>
-        <button className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white">
-          Snakes
-        </button>
+        {categories.map((category, index) => {
+          return (
+            <button
+              key={index}
+              className="font-bold  bg-gray-200 rounded-lg px-3 py-2 hover:bg-green-500 hover:text-white"
+            >
+              {category}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
